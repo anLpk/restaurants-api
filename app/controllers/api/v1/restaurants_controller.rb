f3dfa0640d85a15ab3cb391f75e38affcf87619a
anil pak <anilpak35@gmail.com>
2021-02-18 11:31:43 +1100

    CRUD created for API Call

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,16 +1,40 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, only: [ :show ]
-  def index
-    @restaurants = policy_scope(Restaurant)
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_restaurant, only: [ :show, :update ]
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      render :show, status: :created
+    else
+      render_error
+    end
   end
 
-  def show
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    head :no_content
+    # No need to create a `destroy.json.jbuilder` view
   end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-    authorize @restaurant  # For Pundit
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
